3af310d50571171f9cac581dffe26e46afb22432
blackdog <blackestdoggo@gmail.com>
2022-11-28 19:12:09 +0100

    duzo zmian

--- scripts.js
+++ scripts.js
@@ -1,1 +1,9 @@
-console.log('test');
+jQuery(document).ready(function($) {
+    $("textarea").each(function () { // textarea auto-resize
+        console.log('test');
+        this.setAttribute("style", "height:" + (this.scrollHeight) + "px;overflow-y:hidden;");
+      }).on("input", function () {
+        this.style.height = 0;
+        this.style.height = (this.scrollHeight) + "px";
+    });
+});
