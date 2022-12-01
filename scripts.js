a53883703d1728a15bcb9b034be33a83447a2aac
blackdog <blackestdoggo@gmail.com>
2022-12-01 16:08:24 +0100

    mobile, footer, etc

--- scripts.js
+++ scripts.js
@@ -6,7 +6,7 @@ jQuery(document).ready(function($) {
         this.style.height = (this.scrollHeight) + "px";
     });
 
-    $('.collapse').on('shown.bs.collapse', function(e) {
+    $('.collapse').on('shown.bs.collapse', function(e) { // position correction on open accordion item
       var $card = $(this).closest('.accordion-item');
       var $open = $($(this).data('parent')).find('.collapse.show');
       
@@ -15,9 +15,28 @@ jQuery(document).ready(function($) {
       {
             additionalOffset =  $open.height();
       }
-      console.log('wait');
       $('html,body').animate({
         scrollTop: $card.offset().top - additionalOffset - 15
       }, 150);
     });
+
+    var scrollTop = document.getElementById("scrollTop"); // scroll to top
+    window.onscroll = function(){
+        scrollfunction()
+    };
+    function scrollfunction(){
+        if( document.body.scrollTop > 400 || document.documentElement.scrollTop > 400){
+            scrollTop.style.display = "block";
+        } else {
+            scrollTop.style.display = "none";
+        }
+    }
+    scrollTop.addEventListener("click", function(){
+        window.scrollTo({
+            left: 0,
+            top: 0,
+            behavior: "smooth"
+        })
+    })
+
 });
